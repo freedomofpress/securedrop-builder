062866b0055907762f700574d8cd5050b5e22743
Kushal Das <mail@kushaldas.in>
2018-11-01 21:54:16 +0530

    Adds the initial wheels urls and sha256sums
    We also have the script to create the wheelurls.txt file.

---
+++ scripts/createdownloadurls.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# This script finds the currently available sources
+# and wheels built by SecureDrop team and creates
+# a list of URLs to download them from the s3.
+
+import os
+
+DEV_WHEELS_BUCKET_BASE_URL = "https://dev-bin.ops.securedrop.org/localwheels"
+WHEELS_BUCKET_BASE_URL = os.environ.get(
+    "WHEELS_BUCKET_BASE_URL", DEV_WHEELS_BUCKET_BASE_URL
+)
+
+
+with open("./sha256sums.txt") as fobj:
+    lines = fobj.readlines()
+
+
+def main():
+    "Entry point for the command"
+    filenames = []
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        words = line.split()
+        if len(words) == 2:
+            filenames.append(words[1])
+        else:
+            print("Wrong line: {0}".format(line))
+
+    filenames = list(set(filenames))
+    filenames.sort()
+    for name in filenames:
+        print("{0}/{1}".format(WHEELS_BUCKET_BASE_URL, name))
+
+
+if __name__ == "__main__":
+    main()
