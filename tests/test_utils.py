bd40e0e91ab3446bbd1336c5e4f5cf1320c613c5
Erik Moeller <erik@freedom.press>
2023-09-14 20:31:43 -0400

    Expand tests; include transitive dependency in fixtures

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,33 +13,40 @@ from utils import (
     get_requirements_names_and_versions,
     get_poetry_names_and_versions,
     get_relevant_poetry_dependencies,
+    get_poetry_hashes
 )
 
 PYPROJECT_TOML_PATH = Path(__file__).parent / "fixtures/pyproject.toml"
 POETRY_LOCK_PATH = Path(__file__).parent / "fixtures/poetry.lock"
 REQUIREMENTS_TXT_PATH = Path(__file__).parent / "fixtures/requirements.txt"
 
-
 def test_get_requirements_names_and_versions():
-    expected_output = [("colorama", "0.4.6"), ("cowsay", "6.0")]
-    print(get_requirements_names_and_versions(REQUIREMENTS_TXT_PATH))
+    expected_output = [("beautifulsoup4", "4.12.2"), ("colorama", "0.4.6"), ("cowsay", "6.0"), ("soupsieve", "2.5")]
     assert get_requirements_names_and_versions(REQUIREMENTS_TXT_PATH) == expected_output
 
 
 def test_get_poetry_names_and_versions():
-    expected_output = [("colorama", "0.4.6"), ("cowsay", "6.0")]
+    expected_output = [("beautifulsoup4", "4.12.2"), ("colorama", "0.4.6"), ("cowsay", "6.0"), ("soupsieve", "2.5")]
     output = get_poetry_names_and_versions(POETRY_LOCK_PATH, PYPROJECT_TOML_PATH)
     assert output == expected_output
 
 def test_get_relevant_poetry_dependencies():
-    # TODO: Ensure we have a transitive dependency
     expected_output = [
         "colorama",
         "cowsay",
+        "beautifulsoup4",
+        "soupsieve"
     ]
     output = get_relevant_poetry_dependencies(PYPROJECT_TOML_PATH, POETRY_LOCK_PATH)
     assert sorted(output) == sorted(expected_output)
 
+def test_get_poetry_hashes():
+    output = get_poetry_hashes(POETRY_LOCK_PATH, PYPROJECT_TOML_PATH)
+    expected_keys = ['beautifulsoup4==4.12.2', 'colorama==0.4.6', 'cowsay==6.0', 'soupsieve==2.5']
+
+    assert list(output.keys()) == expected_keys
+    assert all(len(output[dep]) > 0 for dep in output.keys())
+    assert all(all(len(hash) == 64 for hash in output[dep]) for dep in output.keys())
 
 if __name__ == "__main__":
     pytest.main([__file__])
