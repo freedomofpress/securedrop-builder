2db81afe14e96a6311506954907570ea116f36c2
Kunal Mehta <legoktm@debian.org>
2023-12-11 17:59:43 -0500

    Update builds for new monorepo structure
    Remove support for building from tarballs while we're at it, which is
    no longer used.

--- tests/test_reproducible_wheels.py
+++ tests/test_reproducible_wheels.py
@@ -4,17 +4,18 @@ import subprocess
 
 # These are the SDW repositories that we build wheels for.
 REPOS_WITH_WHEELS = [
-    "securedrop-client",
-    "securedrop-export",
-    "securedrop-log",
-    "securedrop-proxy",
+    "client",
+    "export",
+    "log",
+    "proxy",
 ]
 
 
-@pytest.mark.parametrize("repo_name", REPOS_WITH_WHEELS)
-def test_wheel_builds_match_version_control(repo_name):
-    repo_url = f"https://github.com/freedomofpress/{repo_name}"
-    build_cmd = f"./scripts/build-sync-wheels --pkg-dir {repo_url} --project {repo_name} --clobber".split()
+@pytest.mark.parametrize("name", REPOS_WITH_WHEELS)
+def test_wheel_builds_match_version_control(name):
+    subprocess.check_call(["git", "clone", "https://github.com/freedomofpress/securedrop-client", f"/tmp/monorepo-{name}"])
+    build_cmd = f"./scripts/build-sync-wheels --pkg-dir /tmp/monorepo-{name}/{name} --project securedrop-{name}" \
+                " --clobber".split()
     subprocess.check_call(build_cmd)
     # Check for modified files (won't catch new, untracked files)
     subprocess.check_call("git diff --exit-code".split())
