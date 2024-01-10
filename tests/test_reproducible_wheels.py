89754a15d96c6a01911a2ce579746a9d420563de
Kunal Mehta <legoktm@debian.org>
2024-01-09 19:09:26 -0500

    Test that workstation-bootstrap wheels are reproducible too

--- tests/test_reproducible_wheels.py
+++ tests/test_reproducible_wheels.py
@@ -31,3 +31,19 @@ def test_wheel_builds_match_version_control(name):
     # Check for new, untracked files. Test will fail if working copy is dirty
     # in any way, so mostly useful in CI.
     assert subprocess.check_output("git status --porcelain".split()) == b""
+
+
+def test_workstation_bootstrap_wheels():
+    subprocess.check_call(
+        [
+            "./scripts/build-sync-wheels",
+            "--pkg-dir",
+            "./workstation-bootstrap",
+            "--project",
+            "workstation-bootstrap",
+        ]
+    )
+    # Check for modified files (won't catch new, untracked files)
+    subprocess.check_call(["/usr/bin/git", "diff", "--exit-code"])
+    # Check for new, untracked files.
+    assert subprocess.check_output(["/usr/bin/git", "status", "--porcelain"]) == b""
